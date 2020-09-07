6b941ab976b64ab4fbab8bda604fa2cb1306492a
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2020-09-07 11:36:54 +0200

    Add information to relate individual gradients to their parameter

--- backpack/extensions/firstorder/batch_grad_transforms/base.py
+++ backpack/extensions/firstorder/batch_grad_transforms/base.py
@@ -1,3 +1,5 @@
+import weakref
+
 from backpack.extensions.firstorder.base import FirstOrderModuleExtension
 
 
@@ -10,12 +12,16 @@ class BatchGradTransformsBase(FirstOrderModuleExtension):
         batch_grad = self.derivatives.bias_jac_t_mat_prod(
             module, g_inp, g_out, g_out[0], sum_batch=False
         )
+        batch_grad._param_weakref = weakref.ref(module.bias)
+
         return self.apply_transforms(ext, batch_grad)
 
     def weight(self, ext, module, g_inp, g_out, bpQuantities):
         batch_grad = self.derivatives.weight_jac_t_mat_prod(
             module, g_inp, g_out, g_out[0], sum_batch=False
         )
+        batch_grad._param_weakref = weakref.ref(module.weight)
+
         return self.apply_transforms(ext, batch_grad)
 
     def apply_transforms(self, ext, batch_grad):
